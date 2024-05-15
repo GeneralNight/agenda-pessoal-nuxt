fe29d6b7ffe7587ccad9c166f913861f04306ff1
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-15 18:46:54 -0300

    loading and badge components

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  size: string;
+  color: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="border-[6px] rounded-full border-r-transparent animate-spin"
+    :class="[size, color]"
+  ></div>
+</template>
