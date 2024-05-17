65519c7e7b6d57019bab13d6ee3b81324f1e55e2
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-17 14:42:21 -0300

    user table row

---
+++ src/components/UserRow.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import type { Profile } from "~~/models";
+
+const props = defineProps<{
+  user: Profile;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center justify-between px-4 py-6">
+    <span class="font-semibold">{{ user.nome }}</span>
+    <button class="defaultButton flex items-center gap-2">
+      <i class="fi fi-ss-pencil flex"></i>
+      <span> Editar </span>
+    </button>
+  </div>
+</template>
