d675dbe9df75429df4bde3309a2bac6b65198d7a
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-17 14:41:52 -0300

    users page

--- src/pages/dashboard/users.vue
+++ src/pages/dashboard/users.vue
@@ -2,8 +2,88 @@
 definePageMeta({
   layout: "dashboard",
 });
+
+const userStore = useUserStore();
+const { loadingUsers, errorLoadUsers, users } = storeToRefs(userStore);
+
+const query = ref("");
+
+const loadUsers = async () => {
+  await userStore.loadUsers(query.value);
+};
+
+const clear = () => {
+  query.value = "";
+  loadUsers();
+};
+
+watch(
+  query,
+  useDebounceFn(() => {
+    if (!query.value.length) return;
+    loadUsers();
+  }, 1500)
+);
+
+onMounted(() => {
+  loadUsers();
+});
 </script>
 
 <template>
-  <div class="flex flex-col">Users</div>
+  <div class="flex flex-col w-full gap-10">
+    <div class="flex flex-col gap-2">
+      <span class="text-xl font-semibold border-custom-black"> Usuários </span>
+      <div class="flex items-end justify-between gap-10">
+        <div class="col-span flex flex-col gap-1 w-full">
+          <label
+            for="profile.nome"
+            class="text-sm text-custom-black-100 font-semibold"
+          >
+            Termo da pesquisa
+          </label>
+          <div class="flex items-center gap-2">
+            <input type="text" class="defaultInput w-full" v-model="query" />
+            <button @click="clear" v-if="query.length">
+              <i class="fi fi-br-cross flex"></i>
+            </button>
+          </div>
+        </div>
+        <button
+          class="text-white bg-custom-black px-4 py-2.5 rounded-lg transition-all duration-[.2s] hover:bg-custom-blue flex items-center gap-2"
+        >
+          <i class="fi fi-rr-plus flex"></i>
+          <span>Novo</span>
+        </button>
+      </div>
+    </div>
+    <div class="flex flex-col items-center gap-2" v-if="loadingUsers">
+      <Loading :color="'text-custom-black'" :size="'h-6 w-6'" />
+      <span class="text-xl font-semibold"> Carregando usuários... </span>
+    </div>
+    <div class="flex flex-col" v-else>
+      <div class="flex flex-col gap-2 items-center" v-if="errorLoadUsers">
+        <i class="fi fi-ss-not-found text-xl flex"></i>
+        <span class="text-xl font-semibold"> Erro ao carregar usuários </span>
+      </div>
+      <div class="flex flex-col" v-else>
+        <div class="flex flex-col gap-2 items-center" v-if="!users.length">
+          <i class="fi fi-br-404 text-xl flex"></i>
+          <span class="text-xl font-semibold"> Nenhum usuário encontrado </span>
+        </div>
+        <div
+          class="flex flex-col w-full border border-custom-black/50 rounded-lg bg-white"
+          v-else
+        >
+          <UserRow
+            v-for="(user, index) in users"
+            :class="{
+              'border-b border-custom-black/50': index < users.length - 1,
+            }"
+            :user="user"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
