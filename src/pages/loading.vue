212f0ee0da70b29535dda993ea9ca8d7fa750e68
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-19 17:08:48 -0300

    fixes auth

---
+++ src/pages/loading.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex items-center justify-center p-4 w-screen h-screen">
+    <div class="flex flex-col gap-4 items-center">
+      <Loading :size="'h-6 w-6'" :color="'text-custom-black'" />
+      <span class="text-custom-black font-semibold text-lg"
+        >Autenticando...</span
+      >
+    </div>
+  </div>
+</template>
--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,22 +1,33 @@
 export default defineNuxtPlugin(({ hook }) => {
   hook("app:created", async () => {
     const pinia = usePinia();
+    const route = useRoute();
     const authStore = useAuthStore(pinia);
     if (process.client) {
       authStore.keepConnected =
         localStorage.getItem("keepConnected") !== null &&
         localStorage.getItem("keepConnected") === "true";
 
+      if (useRoute().name?.toString().includes("dashboard")) {
+        navigateTo(`/loading?redirect=${useRoute().path}`);
+      }
+
       if (authStore.keepConnected) {
         try {
-          await authStore.login({
-            username: localStorage.getItem("username") ?? "",
-            password: localStorage.getItem("password") ?? "",
-          });
-          useRouter().push("/dashboard");
+          await authStore.login(
+            {
+              username: localStorage.getItem("username") ?? "",
+              password: localStorage.getItem("password") ?? "",
+            },
+            true
+          );
+          navigateTo("/dashboard");
         } catch (error) {
-          useRouter().push("/login");
+          navigateTo("/login");
         }
+      } else {
+        authStore.logout();
+        navigateTo("/login");
       }
     }
   });
